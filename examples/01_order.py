e636b471d0d410a28e490a9f1a8bc5f4693f723c
Lorne Malvo <lmalvo80@gmail.com>
2025-05-23 18:22:53 +0500

    update market constants

--- examples/01_order.py
+++ examples/01_order.py
@@ -1,7 +1,7 @@
 import os
 import asyncio
 from flextrade.flextrade_client import Client
-from flextrade.constants.markets import BASE_MARKET_ETH_USD
+from flextrade.constants.markets import BASE_MARKET_ETH_USD, BASE_MARKET_SOL_USD
 from flextrade.constants.common import ADDRESS_ZERO
 from flextrade.enum import Action
 from dotenv import load_dotenv
@@ -20,22 +20,44 @@ async def main():
   create_market_order = client.private.create_market_order(
     0, BASE_MARKET_ETH_USD, Action.BUY, 10, False, ADDRESS_ZERO
   )
-  print(f'Create market order tx: {create_market_order["tx"].hex()}\n')
+  print(f'Create market ETH/USD order tx: {create_market_order["tx"].hex()}\n')
   await asyncio.sleep(8)
 
   create_order = client.private.create_trigger_order(
     0, BASE_MARKET_ETH_USD, Action.BUY, 20, 3000, True, False)
-  print(f'Create order tx: {create_order["tx"].hex()} id: {create_order["order"]["orderIndex"]}\n')
+  print(f'Create ETH/USD order tx: {create_order["tx"].hex()} id: {create_order["order"]["orderIndex"]}\n')
   await asyncio.sleep(8)
 
   update_order = client.private.update_trigger_order(
     0, create_order["order"]["orderIndex"], Action.SELL, 5, 2700, True, False, ADDRESS_ZERO)
-  print(f'Update order tx: {update_order["tx"].hex()} id: {update_order["order"]["orderIndex"]}\n')
+  print(f'Update ETH/USD order tx: {update_order["tx"].hex()} id: {update_order["order"]["orderIndex"]}\n')
   await asyncio.sleep(20)
 
   cancel_order = client.private.cancel_trigger_order(
     0, BASE_MARKET_ETH_USD, update_order["order"]["orderIndex"])
-  print(f'Cancel order tx: {cancel_order["tx"].hex()} id: {cancel_order["order"]["orderIndex"]}\n')
+  print(f'Cancel ETH/USD order tx: {cancel_order["tx"].hex()} id: {cancel_order["order"]["orderIndex"]}\n')
+  await asyncio.sleep(8)
+
+  # SOL
+  create_market_order = client.private.create_market_order(
+    0, BASE_MARKET_SOL_USD, Action.BUY, 10, False, ADDRESS_ZERO
+  )
+  print(f'Create market SOL/USD order tx: {create_market_order["tx"].hex()}\n')
+  await asyncio.sleep(8)
+
+  create_order = client.private.create_trigger_order(
+    0, BASE_MARKET_SOL_USD, Action.BUY, 20, 3000, True, False)
+  print(f'Create SOL/USD order tx: {create_order["tx"].hex()} id: {create_order["order"]["orderIndex"]}\n')
+  await asyncio.sleep(8)
+
+  update_order = client.private.update_trigger_order(
+    0, create_order["order"]["orderIndex"], Action.SELL, 5, 2700, True, False, ADDRESS_ZERO)
+  print(f'Update SOL/USD order tx: {update_order["tx"].hex()} id: {update_order["order"]["orderIndex"]}\n')
+  await asyncio.sleep(20)
+
+  cancel_order = client.private.cancel_trigger_order(
+    0, BASE_MARKET_SOL_USD, update_order["order"]["orderIndex"])
+  print(f'Cancel SOL/USD order tx: {cancel_order["tx"].hex()} id: {cancel_order["order"]["orderIndex"]}\n')
   await asyncio.sleep(8)
 
 if __name__ == '__main__':
